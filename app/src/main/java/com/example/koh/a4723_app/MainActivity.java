29847cfddd4cedacafea9236e201e8137a67fe1f
heeejoo0518 <heeejoo0518@gmail.com>
2018-08-13 13:46:55 +0900

    test2

--- app/src/main/java/com/example/koh/a4723_app/MainActivity.java
+++ app/src/main/java/com/example/koh/a4723_app/MainActivity.java
@@ -4,14 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    //안녕
-    //나도 테스트
-
 }
