85776281f7ab38941f642d7f56408fe5f41bc98f
hyein15 <hyein305@naver.com>
2018-08-13 14:31:02 +0900

    test

--- app/src/main/java/com/example/koh/a4723_app/MainActivity.java
+++ app/src/main/java/com/example/koh/a4723_app/MainActivity.java
@@ -10,8 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    //안녕
-    //나도 테스트
-
+//ㅓㅏㅓㅏㅓㅐㅓㅐㅓㅏㅏㅓㅓㅏ
 }
