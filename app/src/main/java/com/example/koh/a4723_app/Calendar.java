6f8fb69d00d267bbf8bbf25fde0eb39b9366d4f3
kyungkoh <kyungkoh1328@gmail.com>
2018-08-13 16:17:01 +0900

    calendar _ 1

--- app/src/main/java/com/example/koh/a4723_app/Calendar.java
+++ app/src/main/java/com/example/koh/a4723_app/Calendar.java
@@ -2,13 +2,38 @@ package com.example.koh.a4723_app;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
+import android.widget.CalendarView;
+import android.widget.Toast;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+
 
 public class Calendar extends Activity {
+    CalendarView simpleCalendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        simpleCalendarView = (CalendarView) findViewById(R.id.simpleCalendarView); // get the reference of CalendarView
+        simpleCalendarView.setFocusedMonthDateColor(Color.RED); // set the red color for the dates of  focused month
+        simpleCalendarView.setUnfocusedMonthDateColor(Color.BLUE); // set the yellow color for the dates of an unfocused month
+        simpleCalendarView.setSelectedWeekBackgroundColor(Color.RED); // red color for the selected week's background
+        simpleCalendarView.setWeekSeparatorLineColor(Color.GREEN); // green color for the week separator line
+        // perform setOnDateChangeListener event on CalendarView
+        simpleCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                // display the selected date by using a toast
+                Toast.makeText(getApplicationContext(), dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
 }
