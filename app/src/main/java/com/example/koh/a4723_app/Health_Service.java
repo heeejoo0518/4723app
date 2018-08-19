c6e9a3556d97ba7b0b222ba28c543ece6386dfa1
kyungkoh <kyungkoh1328@gmail.com>
2018-08-19 21:37:14 +0900

    heath_service activity 스피너

--- app/src/main/java/com/example/koh/a4723_app/Health_Service.java
+++ app/src/main/java/com/example/koh/a4723_app/Health_Service.java
@@ -2,13 +2,31 @@ package com.example.koh.a4723_app;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class Health_Service extends Activity {
+public class Health_Service extends Activity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health__service);
-    }
 
+
+        final TextView tv = (TextView)findViewById(R.id.textView1);
+        Spinner s = (Spinner)findViewById(R.id.spinner1);
+        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                tv.setText("선택한 지역은 " +parent.getItemAtPosition(position)+" 입니다");
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+    }
 }
