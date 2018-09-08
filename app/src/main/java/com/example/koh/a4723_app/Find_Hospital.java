ea3015e3a3f7444429526d9df2a0f29e0451f05c
heeejoo0518 <heeejoo0518@gmail.com>
2018-09-08 23:07:46 +0900

    오타수정

--- app/src/main/java/com/example/koh/a4723_app/Find_Hospital.java
+++ app/src/main/java/com/example/koh/a4723_app/Find_Hospital.java
@@ -438,7 +438,7 @@ public class Find_Hospital extends Activity
         //디폴트 위치, Seoul
         LatLng DEFAULT_LOCATION = new LatLng(37.56, 126.97);
         String markerTitle = "위치정보 가져올 수 없음";
-        String markerSnippet = "위치 퍼미션과 GPS 활성 요부 확인하세요";
+        String markerSnippet = "위치 퍼미션과 GPS 활성 여부 확인하세요";
 
 
         if (currentMarker != null) currentMarker.remove();
