8fc2885b4d7c8f1229a426a48dfa4d621a4a287a
heeejoo0518 <heeejoo0518@gmail.com>
2018-09-25 03:02:05 +0900

    db를 main activity에서 저장

--- app/src/main/java/com/example/koh/a4723_app/MainActivity.java
+++ app/src/main/java/com/example/koh/a4723_app/MainActivity.java
@@ -33,13 +33,63 @@ public class MainActivity extends AppCompatActivity {
     public static String data;
     static Button benefit;
     String setText_str = "";
+    SQLiteDatabase db;
+    public void saveBenefits(){//레코드 입력 함수
+        getSharedPreferences("isSaved", Context.MODE_PRIVATE).edit().remove("save").apply();
+        getSharedPreferences("isSaved", Context.MODE_PRIVATE).edit().putInt("save", 1).apply();
+        db.execSQL("CREATE TABLE IF NOT EXISTS YangYang (checked VARCHAR, _start VARCHAR, _end VARCHAR, get VARCHAR(100));");//양양군
+        db.execSQL("CREATE TABLE IF NOT EXISTS JeongSeon (checked VARCHAR, _start VARCHAR, _end VARCHAR, get VARCHAR(100));");//정선군
+        db.execSQL("CREATE TABLE IF NOT EXISTS SamCheok (checked VARCHAR, _start VARCHAR, _end VARCHAR, get VARCHAR(100));");//삼척시
+        db.execSQL("CREATE TABLE IF NOT EXISTS PyeongChang (checked VARCHAR, _start VARCHAR, _end VARCHAR, get VARCHAR(100));");//평창군
+        //==================================================================================양양군
+        db.execSQL("INSERT INTO YangYang (checked, _start, _end ,get) Values ('0','0','64','표준모자보건수첩 제공');");
+        db.execSQL("INSERT INTO YangYang (checked, _start, _end ,get) Values ('0','0','12','엽산제 무료 지급');");
+        db.execSQL("INSERT INTO YangYang (checked, _start, _end ,get) Values ('0','16','48','철분제 무료 제공');");
+        db.execSQL("INSERT INTO YangYang (checked, _start, _end ,get) Values ('0','0','40','유산균제 1회 제공');");
+        db.execSQL("INSERT INTO YangYang (checked, _start, _end ,get) Values ('0','40','44','신생아 청각선별검사');");
+        db.execSQL("INSERT INTO YangYang (checked, _start, _end ,get) Values ('0','40','64','고위험 임산부 의료비 지원');");
+        db.execSQL("INSERT INTO YangYang (checked, _start, _end ,get) Values ('0','34','44','바우처 서비스 신청');");
+        db.execSQL("INSERT INTO YangYang (checked, _start, _end ,get) Values ('0','40','64','의료비 지원사업');");
+        db.execSQL("INSERT INTO YangYang (checked, _start, _end ,get) Values ('0','40','44','출산장려금 지원사업');");
+        db.execSQL("INSERT INTO YangYang (checked, _start, _end ,get) Values ('0','40','48','저소득층 기저귀·조제분유 지원사업');");
+        //==================================================================================정선군
+        db.execSQL("INSERT INTO JeongSeon (checked, _start, _end ,get) Values ('0','0','12','엽산제 제공');");
+        db.execSQL("INSERT INTO JeongSeon (checked, _start, _end ,get) Values ('0','16','40','철분제 제공');");
+        db.execSQL("INSERT INTO JeongSeon (checked, _start, _end ,get) Values ('0','40','44','출산육아용품 상품권 지원');");
+        db.execSQL("INSERT INTO JeongSeon (checked, _start, _end ,get) Values ('0','40','64','미숙아·선천성이상아 의료비지원');");
+        db.execSQL("INSERT INTO JeongSeon (checked, _start, _end ,get) Values ('0','28','44','신생아 청각선별검사 지원');");
+        db.execSQL("INSERT INTO JeongSeon (checked, _start, _end ,get) Values ('0','34','44','산모·신생아건강관리 지원사업');");
+        db.execSQL("INSERT INTO JeongSeon (checked, _start, _end ,get) Values ('0','40','64','고위험임산부 의료비 지원사업');");
+        db.execSQL("INSERT INTO JeongSeon (checked, _start, _end ,get) Values ('0','40','134','저소득층 기저귀·조제분유 지원사업');");
+        //==================================================================================삼척시
+        db.execSQL("INSERT INTO SamCheok (checked, _start, _end ,get) Values ('0','20','40','고위험 임산부 의료비지원 사업');");
+        db.execSQL("INSERT INTO SamCheok (checked, _start, _end ,get) Values ('0','34','44','산모신생아 건강관리사업');");
+        db.execSQL("INSERT INTO SamCheok (checked, _start, _end ,get) Values ('0','40','134','저소득층 기저귀 조제분유 지원사업');");
+        db.execSQL("INSERT INTO SamCheok (checked, _start, _end ,get) Values ('0','28','44','신생아 난청조기진단 검사');");
+        db.execSQL("INSERT INTO SamCheok (checked, _start, _end ,get) Values ('0','40','44','출산장려금 지원');");
+        db.execSQL("INSERT INTO SamCheok (checked, _start, _end ,get) Values ('0','40','44','출생아 건강보장보험료 지원');");
+        db.execSQL("INSERT INTO SamCheok (checked, _start, _end ,get) Values ('0','32','44','공공산후조리원 이용료 지원');");
+        db.execSQL("INSERT INTO SamCheok (checked, _start, _end ,get) Values ('0','0','40','임산부 산전검진비 지원');");
+        //==================================================================================평창군
+        db.execSQL("INSERT INTO PyeongChang (checked, _start, _end ,get) Values ('0','16','40','임산부 산전검진비 지원');");
+        db.execSQL("INSERT INTO PyeongChang (checked, _start, _end ,get) Values ('0','0','12','임산부 산전검진비 지원');");
+        db.execSQL("INSERT INTO PyeongChang (checked, _start, _end ,get) Values ('0','40','64','임산부 산전검진비 지원');");
+        db.execSQL("INSERT INTO PyeongChang (checked, _start, _end ,get) Values ('0','40','134','임산부 산전검진비 지원');");
+        db.execSQL("INSERT INTO PyeongChang (checked, _start, _end ,get) Values ('0','40','48','임산부 산전검진비 지원');");
+        //==================================================================================
 
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
         mContext = this;
+
+        db = this.openOrCreateDatabase("Benefit", MODE_PRIVATE, null);//db이름: Benefit
+        int check_save = getSharedPreferences("isSaved", Context.MODE_PRIVATE).getInt("save",0);
+        if(check_save==0) saveBenefits(); //한번도 DB 저장한 적이 없을 때만 레코드 입력
+        db.close();
 
         //툴바 설정
         toolbar.setTitleTextColor(Color.parseColor("WHITE")); //제목의 칼라
@@ -323,9 +373,7 @@ public class MainActivity extends AppCompatActivity {
         String check = getPreferences("자동계산");
         String due_date = getPreferences("출산날짜");
 
-
         btSet(BtBenefit());
-
 
         if(my_date != ""){
 
