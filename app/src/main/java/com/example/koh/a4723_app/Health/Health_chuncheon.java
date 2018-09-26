512a6c35c7fe1d46b4f0f2408a70c513e293e935
heeejoo0518 <heeejoo0518@gmail.com>
2018-09-26 20:30:42 +0900

    보건사업에 지도(찾아오시는 길), 전화 기능 추가

--- app/src/main/java/com/example/koh/a4723_app/Health/Health_chuncheon.java
+++ app/src/main/java/com/example/koh/a4723_app/Health/Health_chuncheon.java
@@ -1,13 +1,25 @@
 package com.example.koh.a4723_app.Health;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.net.Uri;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.example.koh.a4723_app.R;
@@ -42,8 +54,55 @@ public class Health_chuncheon extends AppCompatActivity {
         _listview = (ListView)findViewById(R.id.health_listview);
 
         _listview.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
-
         _listview.setOnItemClickListener(onItemClickListener);
+
+        ImageButton plus = (ImageButton) findViewById(R.id.map);
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Health_chuncheon.this);
+                LayoutInflater inflater = getLayoutInflater();
+                ImageView dialoglayout = (ImageView) inflater.inflate(R.layout.dialog_layout, null);
+                dialoglayout.setImageResource(R.drawable.map_chuncheon);
+
+                //다이알로그 yes 버튼==
+                builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                //==============
+                builder.setView(dialoglayout);
+                builder.show();
+            }
+        });
+
+        ImageButton phone = (ImageButton) findViewById(R.id.phone);
+        phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int Permission = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE);
+                if(Permission== PackageManager.PERMISSION_GRANTED){
+                    Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:033-250-3550"));
+                    startActivity(callIntent);
+                }
+                else{
+                    if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) getApplicationContext(), "Manifest.permission.CALL_PHONE")){
+                        Snackbar.make(v, "이 앱을 실행하려면 통화 접근 권한이 필요합니다.",
+                                Snackbar.LENGTH_INDEFINITE).setAction("확인", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+
+                                ActivityCompat.requestPermissions( (Activity)getApplicationContext(), new String[]{"Manifest.permission.CALL_PHONE"}, 200);
+                            }
+                        }).show();
+                    }else {
+                        ActivityCompat.requestPermissions((Activity)getApplicationContext(),new String[]{"Manifest.permission.CALL_PHONE"},200);
+                    }
+                }
+            }
+        });
     }
     private AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener()
     {
