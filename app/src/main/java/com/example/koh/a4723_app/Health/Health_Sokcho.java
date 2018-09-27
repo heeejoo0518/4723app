9309d103c48b27f53a2b0978373a7e22ea3b72cb
hyein15 <hyein305@naver.com>
2018-09-27 22:53:24 +0900

    속초시 보건사업 내용 추가

--- app/src/main/java/com/example/koh/a4723_app/Health/Health_Sokcho.java
+++ app/src/main/java/com/example/koh/a4723_app/Health/Health_Sokcho.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -15,48 +14,23 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.koh.a4723_app.MainActivity;
 import com.example.koh.a4723_app.R;
-import com.example.koh.a4723_app.Wellness.WellnessAdapter;
-import com.example.koh.a4723_app.Wellness.WellnessClicked;
-import com.example.koh.a4723_app.Wellness.WellnessItem;
 import com.example.koh.a4723_app.sokcho.sokcho_0;
 import com.example.koh.a4723_app.sokcho.sokcho_1;
-import com.example.koh.a4723_app.sokcho.sokcho_2;
-import com.example.koh.a4723_app.sokcho.sokcho_3;
-import com.example.koh.a4723_app.sokcho.sokcho_4;
-import com.example.koh.a4723_app.sokcho.sokcho_5;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class Health_Sokcho extends AppCompatActivity {
 
     private ListView _listview;
 
-    private String[] items = { "영유아 건강검진", "모성아동건강관리 사업",
-            "영유아 사전예방적 관리", "인구증가 시책사업", "여성과어린이건강증진",
-            "영양보충사업"};
+    private String[] items = {"임신,출산 지원 사업", "보건 사업" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -133,23 +107,6 @@ public class Health_Sokcho extends AppCompatActivity {
             if(position == 1){
                 //setFrag(1);
                 Intent intent = new Intent(Health_Sokcho.this, sokcho_1.class);
-                startActivity(intent);
-            }
-            if(position == 2){
-                //  setFrag(2);
-                Intent intent = new Intent(Health_Sokcho.this, sokcho_2.class);
-                startActivity(intent);
-            }
-            if(position == 3){
-                Intent intent = new Intent(Health_Sokcho.this, sokcho_3.class);
-                startActivity(intent);
-            }
-            if(position == 4){
-                Intent intent = new Intent(Health_Sokcho.this, sokcho_4.class);
-                startActivity(intent);
-            }
-            if(position == 5) {
-                Intent intent = new Intent(Health_Sokcho.this, sokcho_5.class);
                 startActivity(intent);
             }
 
